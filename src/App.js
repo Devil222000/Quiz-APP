d72a2852538aca75d01f91e06b32ca7931fa4fae
Devil222000 <60605399+Devil222000@users.noreply.github.com>
2021-06-29 14:07:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import "./App.css";
 import { useState } from "react";
+import { Navbar, Card, Button,Form } from "react-bootstrap";
 function App() {
   const [count, setCount] = useState(0);
-  const ans = ["ASTON", "KTM", "Engineer"];
+  const ans = [
+    "The blue whale",
+    "0",
+    "The Umbrella Academy season 2",
+    "Mango",
+    "New York",
+  ];
   const data = [
-    { ques: "Choose a car: ", op1: "ASTON", op2: "Lambo" },
-    { ques: "Best Bike: ", op1: "Bullet", op2: "KTM" },
+    {
+      ques: "What's the biggest animal in the world? ",
+      op1: "The blue whale",
+      op2: "Elephant",
+    },
+    { ques: "How many bones does a shark have?", op1: "246", op2: "0" },
+    {
+      ques: "Which was the most streamed show on netflix in 2020?",
+      op1: "The Umbrella Academy season 2",
+      op2: "Lucifer",
+    },
+    {
+      ques: "What fruit takes the scientific name Mangifera indica?",
+      op1: "Mango",
+      op2: "Orange",
+    },
+    {
+      ques: "Which city had the first ever fashion week?",
+      op1: "New York",
+      op2: "Los Angeles",
+    },
   ];
 
   const [marks, setMarks] = useState(null);
@@ -24,27 +50,43 @@ function App() {
   console.log(data);
   return (
     <div className="App">
-      <h1>Welcome to the Quiz</h1>
-      {data.map((t) => (
-        <div>
-          <label>{t.ques}</label>
-          <select onChange={(e) => PointsCounter(e)}>
-            <option>Select</option>
-            <option>{t.op1}</option>
-            <option>{t.op2}</option>
-          </select>
-          <br />
-          <br />
-        </div>
-      ))}
-      <button
-        onClick={(e) => {
-          getmarks(e);
-        }}
-      >
-        Submit
-      </button>
-      <h5>{marks}</h5>
+      <Navbar bg="dark">
+        <Navbar.Brand>
+          <h1>Welcome to the Quiz Page</h1>
+        </Navbar.Brand>
+      </Navbar>
+      <Card bg="secondary">
+        <Card.Body>
+          {data.map((t, i) => (
+            <div key={i}>
+              <Card bg="success">
+                <Card.Body>
+                  <label>
+                    <h3>{t.ques}</h3>{" "}
+                  </label>
+                  <br />
+                  <select onChange={(e) => PointsCounter(e)}>
+                    <option>Select</option>
+                    <option>{t.op1}</option>
+                    <option>{t.op2}</option>
+                  </select>
+                  <br />
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+          <Button
+            variant="primary"
+            onClick={(e) => {
+              getmarks(e);
+            }}
+          >
+            Submit
+          </Button>{" "}
+          <h5>{marks}</h5>
+        </Card.Body>
+      </Card>
+      
     </div>
   );
 }
